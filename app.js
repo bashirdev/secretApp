5e1eb957d893ce22932ed2124e191fef6156dba3
bashir ahammed <myemail@gmail.com>
2020-03-09 23:14:57 +0400

    passportlocalmongoose concept

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 //jshint esversion:6
 require('dotenv').config();
+
 const express = require('express');
+
 const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
-//const encrypt = require('mongoose-encryption');
-// const md5=require('md5');
-const expresssession = require('express-session');
+
+const session= require('express-session');
 const passport =require('passport');
 const passportLocalMongoose =require('passport-local-mongoose');
 
 const app =express();
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
-
-
 
 app.use(session({
    secret:"this is my secret password.",
@@ -23,20 +23,13 @@ app.use(session({
    saveUninitialized: false
   
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser:true,  useUnifiedTopology: true });
 
-//const bcrypt = require('bcrypt');
-//const saltRounds = 10;
-
-//console.log(md5('12356'));
-//console.log(process.env.API_KEY);
-
-
-
-mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser:true});
-
+mongoose.set('useCreateIndex', true);
 
 const userSchema =new mongoose.Schema({
   email:String,
@@ -45,11 +38,12 @@ const userSchema =new mongoose.Schema({
 
 userSchema.plugin(passportLocalMongoose);
 
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-
 const User =mongoose.model('User', userSchema);
+
 passport.use(User.createStrategy());
+
 passport.serializeUser(User.serializeUser());
+
 passport.deserializeUser(User.deserializeUser());
 
 app.get("/" , function(req,res){
@@ -67,8 +61,27 @@ app.get('/register' , function(req,res){
     res.render("register");
 });
 
+app.get('/secrets', function(req,res){
+    if(req.isAuthenticated()){
+        res.render('secrets');
+    }else{
+        res.redirect("/login");
+    }
+});
+
 app.post('/register', function(req,res){
 
+    User.register({usernname:req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate('local')(req,res, function(){
+               res.redirect('/secrets');
+            });
+            
+        }
+    });
    
 });
 
